99b1cb8e85cc84d9d24010783ef29ecb3ca234b0
Florian Dubois <florian.dubois@epitech.eu>
2026-04-16 16:53:32 +0200

    feat: added the management of the backslash inhibitor

--- src/parsing.c
+++ src/parsing.c
@@ -9,9 +9,81 @@
 #include "functions.h"
 #include "lang.h"
 
-static int count(char *str, char *separator)
+static void replace_backslash_with_separator(char **arg, size_t *x,
+    char *separator)
 {
-    int count = 0;
+    char *tmp = NULL;
+
+    arg[*x] = realloc(arg[*x], sizeof(char *) * (strlen(arg[*x]) +
+            strlen(separator)));
+    if (!arg[*x])
+        exit(84);
+    tmp = my_str_concat(arg[*x], separator);
+    free(arg[*x]);
+    arg[*x] = strdup(tmp);
+    if (!arg[*x])
+        exit(84);
+    free(tmp);
+}
+
+static char *check_backslash(char **arg, size_t *x, char *separator)
+{
+    char *tmp = NULL;
+    size_t nb_loop = 0;
+
+    while (arg[*x][strlen(arg[*x]) - 1] == '\\') {
+        if (strlen(separator) > 1)
+            replace_backslash_with_separator(arg, x, separator);
+        else
+            arg[*x - nb_loop][strlen(arg[*x - nb_loop]) - 1] = separator[0];
+        if (!arg[*x + 1])
+            break;
+        else {
+            tmp = my_str_concat(arg[*x - nb_loop], arg[*x + 1]);
+            free(arg[*x - nb_loop]);
+            arg[*x - nb_loop] = strdup(tmp);
+            free(tmp);
+        }
+        *x += 1;
+        nb_loop++;
+    }
+    return arg[*x - nb_loop];
+}
+
+static size_t count_without_backslash(char **arg, size_t count_nb)
+{
+    for (size_t x = 0; arg[x] != NULL; x++)
+        if (arg[x][strlen(arg[x]) - 1] == '\\' && arg[x + 1] != NULL)
+            count_nb--;
+    return count_nb;
+}
+
+static char **check_inhibitors(char **arg, char *separator, size_t count_nb)
+{
+    char **new_arg = NULL;
+    size_t current = 0;
+
+    count_nb = count_without_backslash(arg, count_nb);
+    new_arg = malloc(sizeof(char *) * (count_nb + 1));
+    if (!new_arg)
+        exit(84);
+    for (size_t x = 0; arg[x] != NULL; x++) {
+        if (arg[x][strlen(arg[x]) - 1] == '\\') {
+            new_arg[current] = strdup(check_backslash(arg, &x, separator));
+            current++;
+        } else {
+            new_arg[current] = strdup(arg[x]);
+            current++;
+        }
+    }
+    new_arg[current] = NULL;
+    free_array(arg);
+    return new_arg;
+}
+
+static size_t count(char *str, char *separator)
+{
+    size_t count = 0;
     char *copy = NULL;
     char *token = NULL;
 
@@ -29,7 +101,8 @@ static int count(char *str, char *separator)
 
 char **transform_to_string_array(char *str, char *separator)
 {
-    char **arg = malloc(sizeof(char *) * (count(str, separator) + 1));
+    size_t count_nb = count(str, separator);
+    char **arg = malloc(sizeof(char *) * (count_nb + 1));
     int index = 0;
     char *token = NULL;
 
@@ -44,6 +117,7 @@ char **transform_to_string_array(char *str, char *separator)
         token = strtok(NULL, separator);
     }
     arg[index] = NULL;
+    arg = check_inhibitors(arg, separator, count_nb);
     return arg;
 }
 
